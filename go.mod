2b2470f504b700c093a89e93b440179feb16ba4f
karokojnr <karoko.jnr99@gmail.com>
2024-07-15 11:53:29 +0300

    Added test for Newsletter Signup

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	cloud.google.com/go/compute/metadata v0.3.0 // indirect
 	cloud.google.com/go/iam v1.1.8 // indirect
 	cloud.google.com/go/pubsub v1.40.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
@@ -29,8 +30,10 @@ require (
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.5 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sendgrid/rest v2.6.9+incompatible // indirect
 	github.com/sendgrid/sendgrid-go v3.14.0+incompatible // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.49.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
@@ -47,6 +50,7 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240617180043-68d350f18fd4 // indirect
 	google.golang.org/grpc v1.64.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
@@ -54,6 +58,7 @@ require (
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
+	github.com/stretchr/testify v1.9.0
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
---
+++ tests/newsletter_handler_test.go
@@ -0,0 +1,218 @@
+package tests
+
+import (
+	transportHTTP "MailBeacon/internal/transport/http"
+	"MailBeacon/internal/types"
+	"MailBeacon/internal/utils"
+	"bytes"
+	"context"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+var (
+	token = utils.GenerateRandomToken()
+)
+
+// Store
+type MockStore struct {
+	mock.Mock
+}
+
+func (m *MockStore) AddUser(ctx context.Context, user types.User) error {
+	args := m.Called(ctx, user)
+	return args.Error(0)
+}
+
+func (m *MockStore) ConfirmUser(ctx context.Context, user types.User) error {
+	args := m.Called(ctx, user)
+	return args.Error(0)
+}
+
+// PubSub
+type MockPubSub struct {
+	mock.Mock
+}
+
+func (m *MockPubSub) Publish(topicId string, payload any) error {
+	args := m.Called(topicId, payload)
+	return args.Error(0)
+}
+
+// Mailer
+type MockMailer struct {
+	mock.Mock
+}
+
+func (m *MockMailer) SendConfirmationEmail(email string, token string) error {
+	args := m.Called(email, token)
+	return args.Error(0)
+}
+
+func (m *MockMailer) SendWelcomeEmail(email string) error {
+	args := m.Called(email)
+	return args.Error(0)
+}
+
+type MockService struct {
+	mock.Mock
+}
+
+func (m *MockService) SignUp(ctx context.Context, user types.User) error {
+	user.Token = token
+	args := m.Called(ctx, user)
+	return args.Error(0)
+
+}
+
+func (m *MockService) SendConfirmationEmail(ctx context.Context, user types.User) error {
+	args := m.Called(ctx, user)
+	return args.Error(0)
+}
+
+func (m *MockService) ConfirmSubscription(ctx context.Context, user types.User) error {
+	args := m.Called(ctx, user)
+	return args.Error(0)
+
+}
+
+func (m *MockService) SendWelcomeEmail(ctx context.Context, user types.User) error {
+	args := m.Called(ctx, user)
+	return args.Error(0)
+
+}
+
+func TestNewsletterSignup(t *testing.T) {
+	mockService := new(MockService)
+	handler := transportHTTP.Handler{Service: mockService}
+
+	tests := []struct {
+		name           string
+		email          string
+		mockServiceErr error
+		expectedStatus int
+		expectedBody   string
+	}{
+		{
+			name:           "Kennedy Karoko",
+			email:          "test@example.com",
+			mockServiceErr: nil,
+			expectedStatus: http.StatusOK,
+			expectedBody:   "Thank you for signing up! Please check your email to confirm your subscription.",
+		},
+		// {
+		// 	name:           "invalid email",
+		// 	email:          "",
+		// 	mockServiceErr: nil,
+		// 	expectedStatus: http.StatusBadRequest,
+		// 	expectedBody:   "Invalid email",
+		// },
+		// {
+		// 	name:           "service error",
+		// 	email:          "test@example.com",
+		// 	mockServiceErr: errors.New("internal error"),
+		// 	expectedStatus: http.StatusInternalServerError,
+		// 	expectedBody:   "internal error",
+		// },
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			body := bytes.NewBufferString("email=" + tc.email)
+			req := httptest.NewRequest("POST", "/api/v1/newsletter/signup", body)
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			w := httptest.NewRecorder()
+
+			if tc.email != "" {
+				user := types.User{Email: tc.email, Token: token}
+				mockService.On("SignUp", mock.Anything, user).Return(tc.mockServiceErr)
+			}
+
+			handler.NewsletterSignup(w, req)
+
+			res := w.Result()
+			defer res.Body.Close()
+
+			assert.Equal(t, tc.expectedStatus, res.StatusCode)
+			if tc.expectedBody != "" {
+				buffer := new(bytes.Buffer)
+				buffer.ReadFrom(res.Body)
+				bodyString := buffer.String()
+				assert.Contains(t, bodyString, tc.expectedBody)
+			}
+
+			mockService.AssertExpectations(t)
+		})
+	}
+}
+
+func TestSendConfirmationEmail(t *testing.T) {
+	mockService := new(MockService)
+	handler := transportHTTP.Handler{Service: mockService}
+
+	tests := []struct {
+		name           string
+		payload        string
+		mockServiceErr error
+		expectedStatus int
+		expectedBody   string
+	}{
+		{
+			name:           "valid payload",
+			payload:        `{"message": {"data": "eyJlbWFpbCI6InRlc3RAZXhhbXBsZS5jb20ifQ=="}}`,
+			mockServiceErr: nil,
+			expectedStatus: http.StatusOK,
+			expectedBody:   "Confirmation email sent!",
+		},
+		{
+			name:           "invalid payload",
+			payload:        `{"message": {"data": "invalid"}}`,
+			mockServiceErr: nil,
+			expectedStatus: http.StatusBadRequest,
+			expectedBody:   "Invalid request body",
+		},
+		{
+			name:           "service error",
+			payload:        `{"message": {"data": "eyJlbWFpbCI6InRlc3RAZXhhbXBsZS5jb20ifQ=="}}`,
+			mockServiceErr: errors.New("internal error"),
+			expectedStatus: http.StatusInternalServerError,
+			expectedBody:   "internal error",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			body := bytes.NewBufferString(tc.payload)
+			req := httptest.NewRequest("POST", "/api/v1/newsletter/send-confirmation-email", body)
+			req.Header.Set("Content-Type", "application/json")
+			w := httptest.NewRecorder()
+
+			if tc.payload != "" {
+				user := types.User{Email: "", Token: token}
+				mockService.On("SendConfirmationEmail", mock.Anything, user).Return(tc.mockServiceErr)
+			}
+
+			handler.SendConfirmationEmail(w, req)
+
+			res := w.Result()
+			defer res.Body.Close()
+
+			assert.Equal(t, tc.expectedStatus, res.StatusCode)
+			if tc.expectedBody != "" {
+				buffer := new(bytes.Buffer)
+				buffer.ReadFrom(res.Body)
+				bodyString := buffer.String()
+				assert.Contains(t, bodyString, tc.expectedBody)
+			}
+
+			mockService.AssertExpectations(t)
+
+		})
+	}
+
+}
